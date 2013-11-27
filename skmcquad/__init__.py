23e558c0a03e23309a4dfb3b5b9965da4722f9ec
Pascal Bugnion <pob24@cam.ac.uk>
2013-11-27 16:43:22 +0000

    DOC: Added list of functions to __init__ file.

--- skmcquad/__init__.py
+++ skmcquad/__init__.py
@@ -7,6 +7,8 @@ Monte Carlo integration
 This module provides a toolkit for Monte Carlo integration.
 
     mcquad   -- Integration over a uniformly-sampled hypercube.
+    mcimport -- Integration over points distributed according to a particular pdf.
+    integrate_from_points -- Integration of a function over specific points.
 """
 
 from uniform import * 
