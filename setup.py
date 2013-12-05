7ea5cf72e2fabc131599ad0978ff624a6c349746
Pascal Bugnion <pob24@cam.ac.uk>
2013-12-05 18:24:30 +0000

    BLD: Allow syntax highlighting of LONG_DESCRIPTION in setup.
    Previously, the code snippet in the README file (and hence in the
    LONG_DESCRIPTION keyword) was just an rst literal block. Now it's
    a .. code::python block.
    
    The README file is now automatically generated from the LONG_DESCRIPTION
    keyword, rather than the converse.
    
    Because the README file has to be parsed by
    Github and other programs that don't necessarily understand non-standard
    extensions to rst, the substitution ".. code::python" -> "::" is made
    when generating the Makefile.

--- setup.py
+++ setup.py
@@ -4,11 +4,83 @@ import sys
 
 DISTNAME = "scikit-monaco"
 DESCRIPTION = "Python modules for Monte Carlo integration"
-LONG_DESCRIPTION = open("README.rst").read()
 MAINTAINER = "Pascal Bugnion"
 MAINTAINER_EMAIL = "pascal@bugnion.org"
 URL = "https://pypi.python.org/pypi/scikit-monaco"
 LICENSE = "new BSD"
+
+LONG_DESCRIPTION = """
+
+scikit-monaco
+=============
+
+scikit-monaco is a library for Monte Carlo integration in python. The core is
+written in Cython, with process-level parallelism to squeeze the last bits of
+speed out of the python interpreter.
+
+A code snippet is worth a thousand words. Let's look at integrating 
+``sqrt(x**2 + y**2 + z**2)`` in the unit square:
+
+.. code:: python
+
+    >>> from skmonaco import mcquad
+    >>> from math import sqrt
+    >>> result, error = mcquad(
+    ...     lambda xs: sqrt(xs[0]**2+xs[1]**2+xs[2]**2), 
+    ...     npoints=1e6, xl=[0.,0.,0.], xu=[1.,1.,1.])
+    >>> print "{} +/- {}".format(result,error)
+    0.960695982212 +/- 0.000277843266684
+
+Links
+-----
+
+* Home page: https://pypi.python.org/pypi/scikit-monaco
+* Documentation: http://scikit-monaco.readthedocs.org/en/latest/
+* Source code: https://github.com/scikit-monaco/scikit-monaco
+* Issues: https://github.com/scikit-monaco/scikit-monaco/issues
+
+Installation
+------------
+
+From Pypi
+^^^^^^^^^
+
+The easiest way to download and install scikit-monaco is from the Python
+package index (pypi). Just run::
+
+    $ python easy_install scikit-monaco
+
+Or, if you have pip::
+
+    $ pip install scikit-monaco
+
+From source
+^^^^^^^^^^^
+
+Clone the repository using::
+    
+    $ git clone https://github.com/scikit-monaco/scikit-monaco.git
+
+And run::
+
+    $ python setup.py install
+
+in the project's root directory.
+
+
+Testing
+-------
+
+After the installation, run ``$ python runtests.py`` in the package's root directory.
+
+
+Issue reporting and contributing
+--------------------------------
+
+Report issues using the `github issue tracker <https://github.com/scikit-monaco/scikit-monaco/issues>`_.
+
+Read the CONTRIBUTING guide to learn how to contribute.
+"""
 
 if sys.version_info[0] < 3:
     import __builtin__ as builtins
@@ -21,6 +93,15 @@ else:
 # a lot more robust than what was previously being used.
 # Copied from scipy setup file.
 builtins.__SKMONACO_SETUP__ = True
+
+# Create README file from LONG_DESCRIPTION, replacing non-standard
+# bits of re-structured text.
+with open("README.rst","w") as f:
+    f.write("""\
+.. Automatically generated from LONG_DESCRIPTION keyword in 
+.. setup.py. Do not edit directly.\
+    """)
+    f.write(LONG_DESCRIPTION.replace(".. code:: python","::"))
 
 import skmonaco
 
