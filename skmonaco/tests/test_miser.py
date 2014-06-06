dcb58d06c20b7446c3fb48ecf17adfa1f848aae6
Pascal Bugnion <pob24@cam.ac.uk>
2014-06-06 11:25:08 +0100

    TST: Added tests for arguments to mcmiser.
    Check that the arguments passed to mcmiser make sense.

--- skmonaco/tests/test_miser.py
+++ skmonaco/tests/test_miser.py
@@ -33,6 +33,38 @@ class TestMCMiser(TestCase):
         res2, error2 = mcmiser(func, npoints, [0.], [1.], seed=[1234,5678])
         assert res == res2, "{0} != {1}".format(res, res2)
         assert error == error2
+
+    def test_seed_different(self):
+        """
+        Test different seed -> different result in MISER
+        """
+        func = lambda x: x**2
+        npoints = 50000
+        res, error = mcmiser(func, npoints, [0.], [1.], seed=[1234,5678])
+        res2, error2 = mcmiser(func, npoints, [0.], [1.], seed=[1235,5678])
+        assert res != res2, "{0} == {1}".format(res, res2)
+        assert error != error2
+        
+    def test_wrong_xl(self):
+        """
+        Raise a ValueError if len(xl) != len(xu) in MISER.
+        """
+        with self.assertRaises(ValueError):
+            mcmiser(lambda x: x**2,2000,xl=[0.,0.],xu=[1.])
+
+    def test_wrong_npoints(self):
+        """
+        Raise a ValueError if npoints < 2 in MISER.
+        """
+        with self.assertRaises(ValueError):
+            mcmiser(lambda x: x**2,0,xl=[0.],xu=[1.])
+
+    def test_wrong_nprocs(self):
+        """
+        Raise a ValueError if nprocs < 1
+        """
+        with self.assertRaises(ValueError):
+            mcmiser(lambda x: x**2,2000,xl=[0.],xu=[1.],nprocs=-1)
         
 
 if __name__ == '__main__':
